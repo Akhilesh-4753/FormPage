4a267adc2a738313802cd9c80a827a07af7ba953
Akhilesh <akhipn4753@gmail.com>
2025-11-06 13:31:57 +0530

    Update signup form submission and fetch logic
    Refactor signup form handling and AJAX request.

--- FormPage/wwwroot/js/signUp.js
+++ FormPage/wwwroot/js/signUp.js
@@ -1,27 +1,14 @@
-﻿
-(function () {
 
-        $('#signupForm').on('submit', function (e) {
-            e.preventDefault();
-
-            var name = $('#name').val();
-            var email = $('#email').val();
-            var password = $('#password').val();
-
-            console.log('Signup Details:', { name, email, password });
-        });
-
-})();
-
-
-function fetchRegister() {
+function fetchRegister(bdyData) {
     try {
         return $.ajax({
-            url: "register", 
-            type: "POST", 
-            dataType: "json",   
+            url: "/register",
+            type: "POST",
+            contentType: "application/json", // ✅ tell server you're sending JSON
+            dataType: "text",                // ✅ expect JSON in response
+            data: JSON.stringify(bdyData),  // ✅ convert JS object → JSON string
             success: function (response) {
-                console.log("✅ Data fetched successfully:", response);
+                console.log("✅ Data sent successfully:", response);
             },
             error: function (xhr, status, error) {
                 console.error("❌ Error fetching data:", error);
@@ -34,3 +21,24 @@ function fetchRegister() {
         console.log("error",error)
     }
 }
+async function setRegister() {
+    debugger;
+    const $formSelector = $("form#signupForm");
+
+    const fname = $formSelector.find("#UsrName").val();
+    const UsrEmail = $formSelector.find("#UsrEmail").val();
+    const usrPswrd = $formSelector.find("#Usrpsswrd").val();
+
+    const bdyData = {
+        Name: fname,
+        Email: UsrEmail,
+        Password: usrPswrd
+    }
+
+    const apiResult = await fetchRegister(bdyData);
+}
+
+$(document).on("submit", "#signupForm", function (e) {
+    e.preventDefault();
+    setRegister();
+});
